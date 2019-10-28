6b983929ee9a9744999495b159a02f3493996cf3
Adam Arutyunov <adam.arut@gmail.com>
2019-10-28 23:20:49 +0300

    Updating PyQL module. Starting to draw basic Qt interface.

--- QLAdmin.py
+++ QLAdmin.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class PyQL:
     def __init__(self, connection):
         self.cursor = connection.cursor()
@@ -22,10 +23,43 @@ class PyQL:
     def update(self, table, key, value, row=[], options=""):
         if row:
             condition = "WHERE " + " AND ".join(row)
+        else:
+            condition = ""
 
         execution = f"UPDATE {table} SET {key}={value} {condition} {options}"
+        self.executions.append(execution)
         self.cursor.execute(execution)
 
         return self.cursor.fetchall()
 
-    def select(self, table, )
+    def select(self, table, columns="*", row=[], options=""):
+        if row:
+            condition = "WHERE " + " AND ".join(row)
+        else:
+            condition = ""
+
+        if type(columns) == list:
+            columns = ", ".join(columns)
+
+        execution = f"SELECT {columns} FROM {table} {condition} {options}"
+        self.executions.append(execution)
+        self.cursor.execute(execution)
+
+        return self.cursor.fetchall()
+
+    def commit(self):
+        self.cursor.commit()
+
+    def delete_table(self, table):
+        execution = f"DROP TABLE {table}"
+        self.executions.append(execution)
+        self.cursor.execute(execution)
+
+    def truncate_table(self, table):
+        execution = f"TRUNCATE TABLE {table}"
+        self.executions.append(execution)
+        self.cursor.execute(execution)
+
+
+# connection = sqlite3.connect()
+PQLE = PyQl()
