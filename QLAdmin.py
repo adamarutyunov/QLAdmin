cbefa9bdac4922b932c98af817e2f41e6b1194c5
Adam Arutyunov <adam.arut@gmail.com>
2019-10-29 21:17:55 +0300

    Created QLTableViewWindow class. Minimal features in PyQL.

--- QLAdmin.py
+++ QLAdmin.py
@@ -1,5 +1,8 @@
+import sys
 import sqlite3
-
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QMainWindow
+from PyQt5.QtGui import QWindow
+from PyQt5 import uic
 
 class PyQL:
     def __init__(self, connection):
@@ -60,6 +63,48 @@ class PyQL:
         self.executions.append(execution)
         self.cursor.execute(execution)
 
+    def execute(self, execution):
+        self.cursor.execute(execution)
+        return self.cursor.fetchall()
 
-# connection = sqlite3.connect()
-PQLE = PyQl()
+
+class QLTableViewWindow(QMainWindow):
+    def __init__(self, table_name):
+        super().__init__()
+        uic.loadUi("TableViewWindow.ui", self)
+        self.table_name = table_name
+        self.table_init()
+        self.show()
+
+        self.ok_button.clicked.connect(self.close)
+
+    def table_init(self):
+        table = PQLE.select(self.table_name)
+        column_names = PQLE.execute(f"PRAGMA table_info({self.table_name})")  # this shit must be refactored
+        self.table.setColumnCount(len(column_names))
+        self.table.setHorizontalHeaderLabels(map(lambda x: x[1], column_names))
+        self.table.setRowCount(0)
+        
+        for i, row in enumerate(table):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.table.setItem(i, j, QTableWidgetItem(str(elem)))
+                
+        self.table.resizeColumnsToContents()
+
+    def close(self):
+        self.__del__()
+
+
+class QLAdmin:
+    def __init__(self):
+        self.tablewindow = QLTableViewWindow("Films")
+        #del self.tablewindow
+
+
+connection = sqlite3.connect("films.db")
+PQLE = PyQL(connection)
+
+app = QApplication(sys.argv)
+QLA = QLAdmin()
+sys.exit(app.exec()) 
