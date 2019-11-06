8eeb9d53140bc82b8e272bfce2f144b3e136653a
arutyunov_adam <adam.arut@gmail.com>
2019-11-06 16:58:05 +0300

    Added UIs and classes for column actions in table (add, rename). Added rename tables actions. Changes in UIs. Moved closeEvent to QLWindow class.

--- QLAdmin.py
+++ QLAdmin.py
@@ -11,6 +11,10 @@ class QLWindow:
     def close(self):
         QLA.close(self)
 
+    def closeEvent(self, event):
+        self.close()
+        event.accept()
+
 
 class PyQL:
     def __init__(self, connection):
@@ -91,6 +95,23 @@ class PyQL:
         if not self.executions or execution != self.executions[-1]:
             self.executions.append(execution)
 
+    def add_column(self, table, column_name, data_type, default_value):
+        execution = f"""ALTER TABLE {table}
+                        ADD COLUMN {column_name}
+                        {data_type} DEFAULT {default_value}"""
+        self.executions.append(execution)
+        self.execute(execution)
+
+    def rename_table(self, table, new_name):
+        execution = f"ALTER TABLE {table} RENAME TO {new_name}"
+        self.executions.append(execution)
+        self.execute(execution)
+
+    def rename_column(self, table, column, new_name):
+        execution = f"ALTER TABLE {table} RENAME COLUMN {column} TO {new_name}"
+        self.executions.append(execution)
+        self.execute(execution)
+
 
 class QLTableViewWindow(QMainWindow, QLWindow):
     def __init__(self, table_name):
@@ -155,10 +176,6 @@ class QLTableViewWindow(QMainWindow, QLWindow):
             self.table.item(i, j).setBackground(QColor(200, 200, 200))
         self.table.clearSelection()
 
-    def closeEvent(self, event):
-        self.close()
-        event.accept()
-
 
 class QLDatabaseViewWindow(QMainWindow, QLWindow):
     def __init__(self, database):
@@ -171,6 +188,8 @@ class QLDatabaseViewWindow(QMainWindow, QLWindow):
         self.delete_button.clicked.connect(lambda: self.delete_table(self.get_current_item_text()))
         self.truncate_button.clicked.connect(lambda: self.truncate_table(self.get_current_item_text()))
         self.reindex_button.clicked.connect(lambda: self.reindex_table(self.get_current_item_text()))
+        self.rename_button.clicked.connect(lambda: self.rename_table(self.get_current_item_text()))
+        self.fields_button.clicked.connect(self.view_table_fields)
         self.sql_button.clicked.connect(self.sql_query)
         
         self.tables_list.itemSelectionChanged.connect(self.check_button_state)
@@ -213,11 +232,20 @@ class QLDatabaseViewWindow(QMainWindow, QLWindow):
             self.delete_button.setEnabled(False)
             self.truncate_button.setEnabled(False)
             self.reindex_button.setEnabled(False)
+            self.fields_button.setEnabled(False)
+            self.rename_button.setEnabled(False)
         else:
             self.edit_button.setEnabled(True)
             self.delete_button.setEnabled(True)
             self.truncate_button.setEnabled(True)
             self.reindex_button.setEnabled(True)
+            self.fields_button.setEnabled(True)
+            self.rename_button.setEnabled(True)
+
+    def closeEvent(self, event):
+        self.close()
+        QLA.open_window(QLLoginWindow)
+        event.accept()
 
     def ask_for_commits(self):
         try:
@@ -233,6 +261,16 @@ class QLDatabaseViewWindow(QMainWindow, QLWindow):
 
     def sql_query(self):
         QLA.open_window(QLInputDialog, "Query:", PQLE.execute)
+
+    def view_table_fields(self):
+        table = self.get_current_item_text()
+        QLA.open_window(QLFieldViewWindow, table)
+
+    def rename_table(self, table):
+        QLA.open_window(QLInputDialog, "New name:", lambda new: self.rename(table, new))
+
+    def rename(self, table, new_name):
+        PQLE.rename_table(table, new_name)
 
 class QLAdmin:
     def __init__(self):
@@ -334,7 +372,123 @@ class QLDialog(QDialog, QLWindow):
     def run_function(self):
         self.function()
         self.close()
-    
+
+
+class QLAddFieldWindow(QMainWindow, QLWindow):
+    def __init__(self, table):
+        super().__init__()
+        uic.loadUi("AddFieldWindow.ui", self)
+        self.table = table
+        self.current_type = ""
+        self.setWindowTitle(f"Add field to {self.table}")
+
+        self.init_types()
+        self.fieldtype.activated[str].connect(self.change_type)
+
+        self.cancel_button.clicked.connect(self.close)
+        self.ok_button.clicked.connect(self.add_column)
+
+        self.show()
+
+    def init_types(self):
+        for dtype in data_types:
+            self.fieldtype.addItem(dtype)
+
+    def change_type(self, dtype):
+        self.current_type = dtype
+
+    def add_column(self):
+        column_name = self.fieldname.text()
+        data_type = self.current_type
+        default_value = self.defaultvalue.text()
+        PQLE.add_column(self.table, column_name, data_type, default_value)
+        self.close()
+
+
+class QLFieldViewWindow(QMainWindow, QLWindow):
+    def __init__(self, table):
+        super().__init__()
+        uic.loadUi("FieldViewWindow.ui", self)
+        self.table_name = table
+
+        self.setWindowTitle(f"Fields of {self.table_name}")
+        self.name_label.setText(self.name_label.text().replace('"TableName"', self.table_name))
+
+        self.column_names = PQLE.execute(f"PRAGMA table_info({self.table_name})")
+
+        self.add_column_button.clicked.connect(self.add_column)
+        self.rename_column_button.clicked.connect(self.rename_column)
+
+        self.table.itemSelectionChanged.connect(self.check_button_state)
+
+        self.init_table()
+        self.show()
+
+    def init_table(self):
+        self.table.clearContents()
+        self.table.setColumnCount(2)
+        self.table.setRowCount(0)
+        self.table.setHorizontalHeaderLabels(["Field name", "Field type"])
+
+        self.header = self.table.horizontalHeader()
+
+        for i in range(len(self.header)):
+            self.header.setSectionResizeMode(i, QtWidgets.QHeaderView.Stretch)
+        
+        for i, row in enumerate(self.column_names):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            
+            item_table = QTableWidgetItem(self.column_names[i][1])
+            item_table.setFlags(item_table.flags() ^ QtCore.Qt.ItemIsEditable)
+            self.table.setItem(i, 0, item_table)
+
+            item_table = QTableWidgetItem(self.column_names[i][2])
+            item_table.setFlags(item_table.flags() ^ QtCore.Qt.ItemIsEditable)
+            self.table.setItem(i, 1, item_table)
+
+    def check_button_state(self):
+        if len(self.table.selectedItems()) == 0:
+            self.rename_column_button.setEnabled(False)
+        else:
+            self.rename_column_button.setEnabled(True)
+
+    def rename_column(self):
+        current_column = self.table.item(self.table.currentRow(), 0).text()
+        QLA.open_window(QLFieldRenameWindow, self.table_name, current_column)
+        
+    def add_column(self):
+        QLA.open_window(QLAddFieldWindow, self.table_name)
+
+
+class QLFieldRenameWindow(QDialog, QLWindow):
+    def __init__(self, table, column_name):
+        super().__init__()
+        uic.loadUi("FieldRenameWindow.ui", self)
+
+        self.column_name = column_name
+        self.table_name = table
+
+        self.setWindowTitle(f"Rename field '{column_name}'")
+        self.cancel_button.clicked.connect(self.close)
+        self.ok_button.clicked.connect(self.rename)
+
+        self.label.setText(self.label.text().replace('"FieldName"', self.column_name))
+
+        self.show()
+
+    def rename(self):
+        new_name = self.new_input.text()
+        PQLE.rename_column(self.table_name, self.column_name, new_name)
+        self.close()
+        
+
+data_types = ['INT', 'INTEGER', 'TINYINT', 'SMALLINT', 'MEDIUMINT',
+              'BIGINT', 'UNSIGNED BIG INT', 'INT2', 'INT8', 'CHARACTER(20)',
+              'VARCHAR(255)', 'VARYING CHARACTER(255)', 'NCHAR(55)',
+              'NATIVE CHARACTER(70)', 'NVARCHAR(100)', 'TEXT', 'CLOB',
+              'BLOB', 'REAL', 'DOUBLE', 'DOUBLE PRECISION', 'FLOAT',
+              'NUMERIC', 'DECIMAL(10,5)', 'BOOLEAN', 'DATE', 'DATETIME']
+
 
 
 def excepthook(type, value, tback):
